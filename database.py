fe21deb9d297e0e06402a477eac489e70ca17923
Mino1289 <kretzvictor@orange.fr>
2025-11-01 12:17:55 -0400

    change when using postgre RL

--- database.py
+++ database.py
@@ -5,10 +5,13 @@ from sqlalchemy.orm import sessionmaker
 
 # Utilisation de SQLite avec la base de données events.db
 SQLALCHEMY_DATABASE_URL = "sqlite:///events.db"
+# # Pour PostgreSQL, utilisez la ligne suivante à la place
+# SQLALCHEMY_DATABASE_URL = "postgresql://your_username:your_password@localhost:5432/your_database"
+
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
-    connect_args={"check_same_thread": False}  # Nécessaire pour SQLite
+    connect_args={"check_same_thread": False}  # Nécessaire pour SQLite a enlever pour PostgreSQL
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
